0523de440101bea520d6e0e216ec760fdfc0d52c
Keven CB <kevcb369@gmail.com>
2022-12-20 17:22:09 -0500

    AUpdate: Estilos e isnsninstalacion de bootstrap-icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'boxicons'
 import '../App.css';
 
 const Header = () => {
@@ -7,14 +6,14 @@ const Header = () => {
 		<>
 			<nav className="navbar navbar-dark bg-dark py-4">
 				<div className='container'>
-					<a className="d-flex align-items-center navbar-brand" href="#">Book<box-icon name='book' type='solid' color='#ffffff' ></box-icon>Store</a>
-					<div className='d-flex gap-0'>
-						<button className='cart d-flex align-items-center btn px-1' type='button' data-bs-toggle="offcanvas" data-bs-target="#cart-component" aria-controls="cart-component">
-							<box-icon name='cart' color='#ffffff' ></box-icon>
-							<span className="badge rounded-pill text-bg-danger">1</span>
+					<a className="d-flex align-items-center navbar-brand" href="/#" aria-label='Enlace de prueba'>Book<i className="bi bi-bookmark-heart-fill"></i>Store</a>
+					<div className='d-flex gap-2'>
+						<button className='border border-warning cart d-flex align-items-center gap-1 btn text-ligth' type='button' data-bs-toggle="offcanvas" data-bs-target="#cart-component" aria-controls="cart-component">
+							<i className="text-warning bi bi-cart"></i>
+							<span className="badge rounded-pill text-bg-warning">2</span>
 						</button>
-						<button className='profile d-flex align-items-center btn px-1' type='button' data-bs-toggle="offcanvas" data-bs-target="#profile-component" aria-controls="profile-component">
-							<box-icon name='user-circle' color='#ffffff' ></box-icon>
+						<button className='profile d-flex align-items-center btn btn-warning ' type='button' data-bs-toggle="offcanvas" data-bs-target="#profile-component" aria-controls="profile-component">
+							<i className="bi bi-person-circle"></i>
 						</button>
 					</div>
 				</div>
@@ -23,7 +22,7 @@ const Header = () => {
 			{/* Carrito */}
 			<div className="offcanvas offcanvas-start" tabIndex="-1" id="cart-component" aria-labelledby="cart-componentLabel">
 				<div className="offcanvas-header bg-warning py-4">
-					<a className="d-flex align-items-center navbar-brand text-white" href="#">Book<box-icon name='book' type='solid' color='#ffffff' ></box-icon>Store</a>
+					<a className="d-flex align-items-center navbar-brand text-white" href="/#" aria-label='Enlace de prueba'>Book<i className="text-ligth bi bi-cart"></i>Store</a>
 					<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 				</div>
 				<div className="offcanvas-body">
@@ -34,21 +33,18 @@ const Header = () => {
 			{/* Perfil Usuario */}
 			<div className="offcanvas offcanvas-start" tabIndex="-1" id="profile-component" aria-labelledby="profile-componentLabel">
 				<div className="offcanvas-header bg-dark py-4">
-					<h5 className="text-warning d-flex offcanvas-title" id="profile-componentLabel">
-						¡Hola:
-						<span className="mx-2"><strong>Kev CB!</strong></span>
-					</h5>
+					<a className="d-flex align-items-center navbar-brand text-white" href="/#" aria-hidden="true" aria-label='Enlace de prueba'>Book<i className="bi bi-person-circle"></i>Perfil</a>
 					<button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 				</div>
 				<div className="offcanvas-body mt-2 px-4">
 					<button className="btn list-group-item d-flex align-items-center px-0 py-3 gap-2">
-						<box-icon name='shopping-bag' color='#666666' ></box-icon>Tus pedidos
+						<i className="bi bi-bag"></i>Tus pedidos
 					</button>
 					<button className="btn list-group-item d-flex align-items-center px-0 py-3 gap-2">
-						<box-icon name='star' color="#666"></box-icon>Lista de deseos
+						<i className="bi bi-moon-stars"></i>Lista de deseos
 					</button>
 					<button className="btn list-group-item d-flex align-items-center px-0 py-3 gap-2">
-						<box-icon name='log-out-circle' color='#666666' ></box-icon>Cerrar sesión
+						<i className="bi bi-box-arrow-left"></i>Cerrar sesión
 					</button>
 				</div>
 			</div>
