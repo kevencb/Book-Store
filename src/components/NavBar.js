9c66b1c21388052bc616fbbc7697f166351c44d8
Keven CB <kevcb369@gmail.com>
2022-12-21 17:33:04 -0500

    Update App.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div className='NavBar container my-4 gap-2 d-flex flex-wrap'>
-            <button className="btn btn-sm pb-0 btn-warning" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <div className='container my-4 gap-2 d-flex flex-wrap'>
+            <button className="btn btn-sm btn-warning" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Todos
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Ciencia Ficción
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Derecho
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Filosofía & Religión
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Historía & Arqueología
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Infantiles
             </button>
-            <button className="btn btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Romance
             </button>
         </div>
