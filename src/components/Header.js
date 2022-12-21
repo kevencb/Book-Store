67abfc3b4500cd15b107fe9fb0b57d266eb44e6b
Keven CB <kevcb369@gmail.com>
2022-12-21 17:37:38 -0500

    PreEntrega01-KevenChaparro

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 					<div className='d-flex gap-2'>
 						<button className='border border-warning cart d-flex align-items-center gap-1 btn text-ligth' type='button' data-bs-toggle="offcanvas" data-bs-target="#cart-component" aria-controls="cart-component">
 							<i className="text-warning bi bi-cart"></i>
-							<span className="badge rounded-pill text-bg-warning">2</span>
+							<span className="badge rounded-pill text-bg-warning">4</span>
 						</button>
 						<button className='profile d-flex align-items-center btn btn-warning ' type='button' data-bs-toggle="offcanvas" data-bs-target="#profile-component" aria-controls="profile-component">
 							<i className="bi bi-person-circle"></i>
